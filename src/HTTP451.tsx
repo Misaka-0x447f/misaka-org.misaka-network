c3f353a88529a721968637922bcd40defefe9ca1
御坂云见 <447f@misaka.org>
2025-08-01 20:20:18 +0800

    feat: http451

---
+++ src/HTTP451.tsx
@@ -0,0 +1,49 @@
+import {css} from '@emotion/react'
+
+const HTTP451 = () => {
+    return (
+        <>
+            <div css={css`
+                width: 100%;
+                height: 100%;
+                margin: 0;
+                display: flex;
+                align-content: center;
+                justify-content: center;
+                flex-direction: column;
+                text-align: center;
+            `}>
+                <div css={css`
+                    display: flex;
+                    flex-direction: column;
+                    width: 100%;
+                    margin: auto 0;
+                `}>
+                    HTTP 451 / Unavailable For Legal Reasons
+                    <h1 css={css`
+                        color: #001;
+                        background: #9cf;
+                        padding: 0 0.5em;
+                        line-height: 2.3;
+                        display: inline-block;
+                        font-weight: 100;
+                    `}>
+                        您所在的国家或地区无法使用此服务
+                    </h1>
+                    <div css={css`margin-top: 2em;`}>由于众所周知的原因或/和需要避免不必要的热度，请更换国家或地区后重试。</div>
+                    <div>如果您无法更换国家或地区，请前往 github 寻找并自行部署来源项目。</div>
+                </div>
+                <div css={css`
+                    margin-bottom: 2em;
+                `}>
+                    <div>Compile @ {__BUILT__}</div>
+                    <div>Github:&nbsp;
+                        <a href="https://github.com/Misaka-0x447f">https://github.com/Misaka-0x447f</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HTTP451
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Route, Switch} from "wouter";
+import HTTP451 from "./HTTP451.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Switch>
+          <Route path={'/'}>
+              <App />
+          </Route>
+          <Route path={'/451'}>
+              <HTTP451 />
+          </Route>
+      </Switch>
   </StrictMode>,
 )
