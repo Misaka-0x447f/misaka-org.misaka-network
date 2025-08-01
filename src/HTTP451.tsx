6d3e0d499ffe5789717bd5f1dcb4c573eb6eddbd
御坂云见 <447f@misaka.org>
2025-08-01 20:36:23 +0800

    fix: error text

--- src/HTTP451.tsx
+++ src/HTTP451.tsx
@@ -30,7 +30,7 @@ const HTTP451 = () => {
                     `}>
                         您所在的国家或地区无法使用此服务
                     </h1>
-                    <div css={css`margin-top: 2em;`}>由于众所周知的原因或/和需要避免不必要的热度，请更换国家或地区后重试。</div>
+                    <div css={css`margin-top: 2em;`}>由于众所周知的原因或/和需要避免不必要的热度，请为 ip.misaka.org 更换国家或地区后重试。</div>
                     <div>如果您无法更换国家或地区，请前往 github 寻找并自行部署来源项目。</div>
                 </div>
                 <div css={css`
